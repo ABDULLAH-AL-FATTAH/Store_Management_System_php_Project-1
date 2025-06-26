9b766c40c0a376db9be10ba0b76dbde7a084c88e
ABDULLAH AL FATTAH <fattahfcibd@gmail.com>
2025-06-27 00:44:13 +0600

    edit category sql updated

--- edit_category.php
+++ edit_category.php
@@ -5,14 +5,11 @@
 
 
 <?php 
-    
-  
-
-
+// Task:1 Edit Category Page
+// This page is used to edit the category information in the database.
+	require('connection.php');
 
  ?>
-
-
 
 <!DOCTYPE html>
 
@@ -24,6 +21,7 @@
 
 
 		<?php
+
 			if (isset($_GET['id'])) {
 
 				$getid = $_GET['id'];
@@ -40,6 +38,22 @@
 				$category_name = $data['category_name'];
 				$category_entrydate = $data['category_entrydate'];
 			}
+
+			if (isset($_GET['category_name'])) 
+			{
+				$new_category_name = $_GET['category_name'];
+				$new_category_entrydate = $_GET['category_entrydate'];
+				$new_category_id = $_GET['category_id'];
+
+				// Update the category information in the database
+				$sql = "UPDATE category SET category_name='$new_category_name', category_entrydate='$new_category_entrydate' WHERE category_id=$new_category_id";
+
+				if ($conn->query($sql) === TRUE) {
+				echo 'Data Updated Successfully!';
+				} else {
+				echo 'Data Not Updated';
+				}
+   }
 			
 			
 		?>
@@ -49,10 +63,11 @@
 <!-----------------------------Task:1  form for input data start-------------------------------------------->
 		<form action="edit_category.php" method="GET">
 			Category :<br>
-			<input type="text" name="category_name" value="<?php echo category_name"><br><br>
+			<input type="text" name="category_name" value="<?php echo $category_name ?>"><br><br>
 			Category Entry Date : <br>
-			<input type="date" name="category_entrydate"><br><br>
-			<input type="submit" value="submit">
+			<input type="date" name="category_entrydate" value="<?php echo $category_entrydate ?>"><br><br>
+			<input type="text" name="category_id" value="<?php echo $category_id ?>" hidden>
+			<input type="submit" value="update">
 		</form>
 <!-----------------------------Task:1 form for input data End------------------------------------------>
 	</body>
