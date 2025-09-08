99b9840d0e39163dca140dbeb2ae0441dad96c55
ABDULLAH AL FATTAH <fattahfcibd@gmail.com>
2025-09-09 01:00:41 +0600

    add myfunction and complete the store product

--- add_store_product.php
+++ add_store_product.php
@@ -5,7 +5,9 @@
     //php 'include' and 'require' topic should be understand from the following link https://www.w3schools.com/php/php_includes.asp // 
     
     require('connection.php');
- ?>
+    require('myfunction.php');
+
+?>
 
 
 
@@ -13,7 +15,7 @@
 
 <html>
 	<head>
-		<title>Add Product</title>
+		<title>store Product</title>
 	</head>
 	<body>
 
@@ -21,17 +23,17 @@
 
 
 		<?php
-			if(isset($_GET['product_name']))
+			if(isset($_GET['store_product_name']))
 			{
-    			$product_name	    = $_GET['product_name'];
-    			$product_category   = $_GET['product_category'];
-    			$product_code       = $_GET['product_code'];
-    			$product_entry_date = $_GET['product_entry_date'];
+    			$store_product_name	    = $_GET['store_product_name'];
+    			$store_product_quantity   = $_GET['store_product_quantity'];
+    			$store_product_entry_date       = $_GET['store_product_entry_date'];
+    			
 
 			//now we are going to entry data for above categories of the Category table in our phpmysql using SQL
 
-			     $sql= "INSERT INTO product (product_name, product_category, product_code, product_entry_date) 
-                        VALUES ('$product_name', '$product_category', '$product_code', '$product_entry_date')";
+			     $sql= "INSERT INTO store_product (store_product_name, store_product_quantity, store_product_entry_date) 
+                        VALUES ('$store_product_name', '$store_product_quantity', '$store_product_entry_date')";
 			//Now we have create some quories using php's OOP method follow this: 
 
     				if ($conn->query($sql) == TRUE)
@@ -47,41 +49,27 @@
 		?>
 
 
-        <!-- Task-2 database table insert by OOP and SQL insert start----------------->
-
-<!-- category তে entry করা data গুলো এখানে list আকারে দেখানোর জন্য নিচের কোডটি ব্যবহার করা হয়েছে।  -->
- <?php
-    $sql = "SELECT * FROM category";
-    $query = $conn->query($sql);
- ?>
-
 
         
 <!-----------------------------Task:1  form for input data start-------------------------------------------->
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+			
 			Product :<br>
-			<input type="text" name="product_name"><br><br>
-
-			Product Category:<br>
-            <select name="product_category">
+            <select name="store_product_name">
                 <?php
-                    while ($data = mysqli_fetch_array($query))
-                        {
-                                $category_id = $data['category_id'];
-                                $category_name = $data['category_name'];
-                        
-                            echo"<option value='$category_id'>$category_name</option>";
-                        }
+                    
+                    data_list('product', 'product_id', 'product_name');
+
                 ?>
                 
                 
 
             </select><br><br>
 			
-			Product code:<br> 
-			<input type="text" name="product_code"><br><br>
+			Product Quantity:<br> 
+			<input type="text" name="store_product_quantity"><br><br>
 			Product Entry Date : <br>
-			<input type="date" name="product_entry_date"><br><br>
+			<input type="date" name="store_product_entry_date"><br><br>
 
             <input type="submit" value="submit">
 		</form>
