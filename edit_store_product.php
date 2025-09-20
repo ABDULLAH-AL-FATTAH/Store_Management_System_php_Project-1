1bca35668e600f21eefb556d71ef261cdeaa2d6f
ABDULLAH AL FATTAH <fattahfcibd@gmail.com>
2025-09-20 12:22:58 +0600

    add list of entry product and add edit store product

---
+++ edit_store_product.php
@@ -0,0 +1,118 @@
+
+
+<?php 
+    
+    //php 'include' and 'require' topic should be understand from the following link https://www.w3schools.com/php/php_includes.asp // 
+    
+    require('connection.php');
+    require('myfunction.php');
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Edit store Product</title>
+	</head>
+	<body>
+
+        <!-- Task-2 database table insert by OOP and SQL insert start----------------->
+
+
+		<?php
+			if(isset($_GET['id'])){
+                $getid  =   $_GET['id'];
+
+                $sql = "SELECT *FROM store_product WHERE store_product_id  = $getid";
+
+				$query   = $conn->query($sql);
+
+				$data = mysqli_fetch_assoc($query);
+
+
+
+				$store_product_id	            = $data['store_product_id'];
+				$store_product_name             = $data['store_product_name'];
+				$store_product_quantity         = $data['store_product_quantity'];
+				$store_product_entry_date       = $data['store_product_entry_date'];
+            }
+
+            if(isset($_GET['store_product_name']))
+                {
+
+                    $new_store_product_name           = $_GET['store_product_name'];
+                    $new_store_product_quantity       = $_GET['store_product_quantity'];               
+                    $new_store_product_entry_date     = $_GET['store_product_entry_date'];
+                    $new_store_product_id             = $_GET['store_product_id'];
+
+                $sql1 = "UPDATE store_product SET  store_product_name='$new_store_product_name', 
+                                            store_product_quantity='$new_store_product_quantity',
+                                            store_product_entry_date='$new_store_product_entry_date'
+                  WHERE store_product_id=$new_store_product_id";
+
+
+                // $query1 = $conn->query($sql1);
+
+                if($conn->query($sql1)===TRUE){
+                    echo 'Data Updated Successfully!';
+                }
+                else{
+                    echo 'Not Updated';
+                }
+            }
+
+
+		?>
+
+
+
+        
+<!-----------------------------Task:1  form for input data start-------------------------------------------->
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+			
+			Product :<br>
+            <select name="store_product_name">
+                <?php
+                    
+                     $sql = "SELECT * FROM product";
+                     $query = $conn->query($sql);
+
+
+                    while ($data = mysqli_fetch_array($query))
+                        {
+                                $data_id = $data['product_id'];
+                                $data_name = $data['product_name'];
+                   ?>     
+                    <option value='<?php echo $data_id ?>' <?php if($store_product_name == $data_id) {echo 'Selected';} ?> >
+                    
+                    <?php echo $data_name ?>
+                    
+                    </option>
+                    <?php   
+                     }
+                ?>
+                
+                
+
+            </select><br><br>
+			
+			Product Quantity:<br> 
+			<input type="number" name="store_product_quantity" value="<?php echo $store_product_quantity; ?>">     <br><br>
+			Product Entry Date : <br>
+			<input type="date" name="store_product_entry_date" value="<?php echo $store_product_entry_date; ?>"> <br><br>
+
+
+			<input type="date" name="store_product_id" value="<?php echo $store_product_id ?>"Hidden> 
+
+            <input type="submit" value="submit">
+		</form>
+<!-----------------------------Task:1 form for input data End------------------------------------------>
+	</body>
+
+
+
+<!-----------------------------Task:1 form for input data End----------------------------------->
+</html>
---
+++ list_of_entry_product.php
@@ -0,0 +1,72 @@
+
+<?php 
+	
+	//php 'include' and 'require' topic should be understand from the following link https://www.w3schools.com/php/php_includes.asp // 
+	
+	require('connection.php');
+
+        $sql1    =   "SELECT * FROM product";
+        $query1  =   $conn->query($sql1);
+
+        $data_list = array();
+
+       while ($data1   =   mysqli_fetch_assoc($query1))
+       {
+
+            $product_id       =   $data1['product_id'];
+            $product_name     =   $data1['product_name'];
+
+            $data_list[$product_id] = $product_name;
+            
+       }
+        
+      
+ ?>
+
+  <!-----------------------------Task:4.1 php to mysql database connection php segment End ----------------------------------------->				
+
+	<!---------------------php to mysql database connection end------------------->
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>List of Store Product</title>
+	</head>
+	<body>
+			<!-----------------------------Task:4.2 Now we are going to show the Information of the database ----------------------->	
+		<?php
+			//here we use php mysql Database 'SELECT Data'> Example(MySQLi Object-Oriented) link: https://www.w3schools.com/php/php_mysql_select.asp (follow the link to understand)
+			
+
+			$sql="SELECT * FROM store_product";
+
+			$query = $conn->query($sql);
+
+			echo "<table border='1'>
+                <tr><th>Product Name</th><th>Quantity</th><th>Entry Date</th><th>Action</th></tr>";
+
+			while ( $data = mysqli_fetch_assoc($query)) {
+				 
+				 $store_product_id	            = $data['store_product_id'];
+				 $store_product_name            = $data['store_product_name'];
+				 $store_product_quantity        = $data['store_product_quantity'];
+				 $store_product_entry_date      = $data['store_product_entry_date'];
+
+				 echo "<tr>
+				 			<td>$data_list[$store_product_name]</td>
+                            <td>$store_product_quantity</td>
+				 			<td>$store_product_entry_date</td>
+				 			<td><a href='edit_store_product.php?id=$store_product_id'>Edit</a></td>
+				 	  </tr>"; 
+				 	  //here we've created one more coloum as Edit and linking it's associative php file that edit_category.php
+			}
+
+			echo "</table>";
+		?>
+	</body>
+
+
+
+</html>
